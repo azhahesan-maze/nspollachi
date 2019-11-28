a42e561463823687f8fa1df5abccdba05b1f709e
sounderrajan <sounderrajan@mazenetsolution.com>
2019-11-28 11:49:45 +0530

    Nov -28 Morning Location Master Corrections

--- resources/views/admin/master/location/view.blade.php
+++ resources/views/admin/master/location/view.blade.php
@@ -35,7 +35,32 @@
               <td>{{ $value->name}}</td>
               <td>{{ $value->location_type_id}}</td>
               <td>
+                  @if($value->address_line_1 != "")
+                  {{ $value->address_line_1 }},</br>
+                  @endif
+                  @if($value->address_line_2 != "")
+                  {{ $value->address_line_2 }},</br>
+                  @endif
                   
+                
+                  @if($value->land_mark != "")
+                  {{ $value->land_mark }},
+                  @endif
+                  @if(isset($value->city->name) && $value->city->name != "")
+                  {{ $value->city->name }},
+                  @endif
+
+                  @if(isset($value->district->name) && $value->district->name != "")
+                  {{ $value->district->name }},
+                  @endif
+</br>
+
+                  @if(isset($value->state->name) && $value->state->name != "")
+                  {{ $value->state->name }}
+                  @endif
+                  @if($value->postal_code != "")
+                 - {{ $value->postal_code }},</br>
+                  @endif
 
                 
 
