508b1d5df3a3a10c8feed404433e982f3e80f2f6
azhahesan-maze <azhahesan@mazenetsolution.com>
2020-01-09 10:24:59 +0530

    slight changes

--- resources/views/admin/master/item/add.blade.php
+++ resources/views/admin/master/item/add.blade.php
@@ -6,7 +6,7 @@
     <div class="card-header px-2">
       <div class="row">
         <div class="col-4">
-          <h3>Add Item</h3>
+          <h3>Add Items</h3>
         </div>
         <div class="col-8 mr-auto">
           <ul class="h-right-btn mb-0 pl-0">
